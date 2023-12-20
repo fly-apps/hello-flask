28a3029353ca98a6413f02aad714bb2936e921ce
jakeyr <github@redlightbandits.com>
2023-12-20 00:12:05 +0000

    adds inbound email processing and dockerizes

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# pull official base image
+FROM python
+
+# set work directory
+WORKDIR /usr/src/app
+
+# set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+# install dependencies
+RUN apt-get update && \
+    apt-get install -y poppler-utils
+RUN pip install --upgrade pip
+COPY ./requirements.txt /usr/src/app/requirements.txt
+RUN pip install -r requirements.txt
+
+# copy project
+COPY . /usr/src/app/
+
+CMD ["gunicorn", "app:app"]
--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import requests
 import base64
 import os
+import io
 import json
 import datetime
 import logging
 import http
-from pprint import pformat
+from pprint import pformat,pprint
 from pytz import timezone
-from flask import Flask, request, redirect, Response
+from flask import Flask, request, redirect, Response, send_file, render_template, make_response
 from dotenv import load_dotenv
 from openai import OpenAI
+from pdf2image import convert_from_path, convert_from_bytes
+
 
 #http.client.HTTPConnection.debuglevel = 1
 
@@ -19,7 +22,7 @@ app = Flask(__name__)
 
 mock_task = json.loads('{"title": "Plan Hot Air Balloon Trip", "dueDate": "2023-12-16T00:00:00-08:00", "content": "- [ ] Research local hot air balloon companies\\n- [ ] Compare pricing and reviews\\n - [ ] Check the weather forecast for this weekend\\n - [ ] Contact chosen company to book a flight\\n - [ ] Confirm booking and payment details\\n - [ ] Prepare camera equipment for aerial photos\\n - [ ] Notify participants of the trip details", "startDate": "2023-12-15T00:00:00-08:00", "timeZone": "America/Los_Angeles", "isAllDay": true}')
 
-def convert_image_to_task(encoded_image: str) -> dict:
+def convert_image_to_task(encoded_images: [str], query_suffix="") -> dict:
     """
     Converts an encoded image into a task by sending to OpeNAI to analyzing 
     the image, generating a summary of the image and generating a TickTick 
@@ -36,16 +39,12 @@ def convert_image_to_task(encoded_image: str) -> dict:
     """
     client = OpenAI()
 
-    prompt_messages = [
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "text",
-                    "text": """
+    query = f"""
 Take on the persona of my personal assistant.
 
-Please analyze this image and respond with a task in the format of a task for me to do.
+Please analyze the following email. Consider any uploaded images or documents as attachments 
+to the email. Act as my personal assistant would, thinking critically about the purpose and 
+content of the email and the attachments, and respond with a task for me to do.
 
 Format your response as TickTick task in JSON format that can be sent to ticktick via their api.
 Only send the json in your response, do not return any non-json text, numbering, or formatting:
@@ -58,17 +57,29 @@ Only send the json in your response, do not return any non-json text, numbering,
 
 Use markdown formatting only in the task content, no HTML.
 Do not include the string "\n```json" in your response.
+
+{query_suffix}
 """
-                },
+
+    prompt_messages = [
+        {
+            "role": "user",
+            "content": [
                 {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/jpeg;base64,{encoded_image}"
-                    }
+                    "type": "text",
+                    "text": query
                 }
             ]
         }
     ]
+
+    for encoded_image in encoded_images:
+        prompt_messages[0]['content'].append({
+            "type": "image_url",
+            "image_url": {
+                "url": f"data:image/jpeg;base64,{encoded_image}"
+            }
+        })
 
     # Send a request to GPT
     params = {
@@ -126,9 +137,100 @@ def task_from_image():
     task = mock_task
 
     if 'mock' not in request.form:
-        task = convert_image_to_task(request.form['image'])
+        task = convert_image_to_task([request.form['image']])
 
     return create_ticktick_task(task)
+
+
+def get_image_in_jpeg64(image):
+    fn = "./static/tmptest.jpg"
+    r = ""
+    image.save(fn)
+    with open(fn, 'rb') as fp:
+        r = base64.b64encode(fp.read()).decode('utf-8')
+        os.remove(fn)
+
+    return r
+
+def get_query_suffix(html):
+    return f"""
+Here is the email content:
+-------
+{html}
+"""
+
+# cloudmailin version
+# @app.route('/', methods=['POST'])
+# @app.route('/task-from-email-cloudmailin', methods=['POST'])
+# def task_from_email_cloudmailin():
+#     pprint(request.form['attachment1'])
+#     file_bytes = base64.b64decode(request.json['attachments'][0]['content'])
+#     images: List[Image.Image] = convert_from_bytes(file_bytes,fmt="JPEG")
+#     encoded_images = [get_image_in_jpeg64(im) for im in images]
+
+#     if 'mock' not in request.json:
+#         task = convert_image_to_task(encoded_images, query_suffix=get_query_suffix(request.json['html']))
+
+#     pprint(task)
+
+#     return create_ticktick_task(task)
+
+
+# sendgrid version
+# @app.route('/', methods=['POST'])
+# @app.route('/task-from-email', methods=['POST'])
+# def task_from_email_form():
+#     encoded_images = []
+
+#     pprint(request.files)
+    
+#     for attachment in request.files.values():
+#         attachment.save('static/test.pdf')
+#         attachment.seek(0)
+#         file_bytes = attachment.read()
+#         images: List[Image.Image] = convert_from_bytes(file_bytes,fmt="JPEG")
+#         encoded_images.append([get_image_in_jpeg64(im) for im in images])
+
+#     return "OK"
+
+#     print(len(encoded_images))
+
+#     if 'mock' not in request.form:
+#         task = convert_image_to_task(encoded_images, query_suffix=get_query_suffix(request.form['html']))
+
+#     return create_ticktick_task(task)
+
+@app.route('/', methods=['POST'])
+@app.route('/task-from-email-json', methods=['POST'])
+def task_from_email():
+
+    encoded_images = []
+
+    def convert_pdf_to_jpeg64(content):
+        file_bytes = base64.b64decode(content)
+        images: List[Image.Image] = convert_from_bytes(file_bytes,fmt="JPEG")
+        encoded_images = [get_image_in_jpeg64(im) for im in images]
+        return encoded_images
+
+    def convert_jpeg_to_jpeg64(content):
+        return base64.b64decode(content)
+
+    processors = {
+        'application/pdf': convert_pdf_to_jpeg64,
+        'application/jpeg': convert_jpeg_to_jpeg64
+    }
+
+    for attachment in request.json['Attachments']:
+        encoded_images.append(processors[attachment['Content-Type:']](attachment['Content']))
+
+    if 'mock' not in request.json:
+        task = convert_image_to_task(encoded_images, query_suffix=get_query_suffix(request.json['TextBody']))
+
+    pprint(task)
+
+    return create_ticktick_task(task)
+    
+
 
 if __name__ == '__main__':
     app.run()
